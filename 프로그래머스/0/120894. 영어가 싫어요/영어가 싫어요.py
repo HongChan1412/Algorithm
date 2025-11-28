5b07e27f10b559d4d0ac87ffc3a40f2c62d8258c
nahong_c <95984922+HongChan1412@users.noreply.github.com>
2025-11-29 05:03:51 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.04 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,32 @@
+def solution(numbers):
+    answer = 0
+    answer = ""
+    numbers_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    temp = ""
+    for i in numbers:
+        temp += i
+        print(temp)
+        if temp in numbers_list:
+            if temp == "zero":
+                answer += "0"
+            elif temp == "one":
+                answer += "1"
+            elif temp == "two":
+                answer += "2"
+            elif temp == "three":
+                answer += "3"
+            elif temp == "four":
+                answer += "4"
+            elif temp == "five":
+                answer += "5"
+            elif temp == "six":
+                answer += "6"
+            elif temp == "seven":
+                answer += "7"
+            elif temp == "eight":
+                answer += "8"
+            elif temp == "nine":
+                answer += "9"
+            temp = ""
+    answer = int(answer)
+    return answer
