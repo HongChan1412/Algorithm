8441da069cc12e1e3627529ab2f0d1fb4b1d8167
nahong_c <95984922+HongChan1412@users.noreply.github.com>
2025-12-02 09:15:15 +0900

    [level 0] Title: 배열 조각하기, Time: 1.58 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -1,9 +1,3 @@
 def solution(arr, query):
     answer = []
-    answer = arr
-    for idx, val in enumerate(query):
-        if idx % 2 == 0:
-            answer = answer[:val+1]
-        else:
-            answer = answer[val:]
     return answer
